7010b17311c306ef5b82491dda33262819c3f8a4
unknown <sgrsn1711@gmail.com>
2021-08-19 22:47:18 +0900

    one

---
+++ ususama_serial_demo/ususama_serial_demo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ususama_serial;
+
+namespace ususama_serial_demo
+{
+  /// <summary>
+  /// MainWindow.xaml の相互作用ロジック
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public UsusamaController ususama;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void button_Click(object sender, RoutedEventArgs e)
+    {
+      ususama = new UsusamaController();
+    }
+
+    private void button_Copy_Click(object sender, RoutedEventArgs e)
+    {
+      for (int i = 0; i < 30; i++)
+      {
+        ususama.demo(i*100, 10);
+        ususama.ReceiveData();
+      }
+    }
+
+    private void button2_Click(object sender, RoutedEventArgs e)
+    {
+      ususama.CloseInterface();
+    }
+  }
+}
---
+++ ususama_serial_demo/ususama_serial_demo/ususama_serial.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace ususama_serial
+{
+  public class UsusamaController
+  {
+    private UsusamaInterface my_interface;
+    private int[] register = new int[256];
+    public UsusamaController()
+    {
+      my_interface = new UsusamaSerial("COM5", 115200);
+    }
+
+    public void ReceiveData()
+    {
+      byte[] buffer = UsusamaProtocol.ReceiveDataUntilHeader(my_interface);
+      UsusamaProtocol.UsusamaData data_t = UsusamaProtocol.ProcessingReceivedData(buffer);
+      Console.WriteLine("{0}, {1}, {2}", data_t.data, data_t.reg, data_t.valid);
+      if (data_t.valid) register[data_t.reg] = data_t.data;
+    }
+
+    public void demo(int data, byte reg)
+    {
+      UsusamaProtocol.UsusamaData data_t;
+      data_t.data = data;
+      data_t.reg = reg;
+      data_t.valid = true;
+      UsusamaProtocol.SendPacketData(my_interface, data_t);
+    }
+
+    public void CloseInterface()
+    {
+      my_interface.Close();
+    }
+  }
+
+  public static class UsusamaProtocol
+  {
+    private const byte HEAD_BYTE = 0x1D;
+    private const byte ESCAPE_BYTE = 0x1E;
+    private const byte ESCAPE_MASK = 0x1F;
+
+    public struct UsusamaData
+    {
+      public int data;
+      public byte reg;
+      public bool valid;
+    }
+
+    public static void SendPacketData(UsusamaInterface my_interface, UsusamaData data_t)
+    {
+      byte[] dataBytes = new byte[4]
+      {
+        (byte)((data_t.data >> 24) & 0xFF),
+        (byte)((data_t.data >> 16) & 0xFF),
+        (byte)((data_t.data >>  8) & 0xFF),
+        (byte)((data_t.data >>  0) & 0xFF)
+      };
+
+      List<byte> buffer_w = new List<byte>();
+
+      //my_interface.SendByteData(HEAD_BYTE);
+      //my_interface.SendByteData(data_t.reg);
+      buffer_w.Add(HEAD_BYTE);
+      buffer_w.Add(data_t.reg);
+
+      byte checksum = 0;
+      for (int i = 0; i < 4; ++i)
+      {
+        if ((dataBytes[i] == ESCAPE_BYTE) || (dataBytes[i] == HEAD_BYTE))
+        {
+          //my_interface.SendByteData(ESCAPE_BYTE);
+          buffer_w.Add(ESCAPE_BYTE);
+          checksum += ESCAPE_BYTE;
+          //my_interface.SendByteData((byte)((int)dataBytes[i] ^ (int)ESCAPE_MASK));
+          buffer_w.Add((byte)((int)dataBytes[i] ^ (int)ESCAPE_MASK));
+          checksum += (byte)((int)dataBytes[i] ^ (int)ESCAPE_MASK);
+        }
+        else
+        {
+          //my_interface.SendByteData(dataBytes[i]);
+          buffer_w.Add(dataBytes[i]);
+          checksum += dataBytes[i];
+        }
+      }
+      // 末尾にチェックサムを追加で送信する
+      //my_interface.SendByteData(checksum);
+      buffer_w.Add(checksum);
+      int size = buffer_w.Count - 1;
+      buffer_w.Insert(1, (byte)size);
+
+      byte[] buffer = buffer_w.ToArray();
+      my_interface.SendBytes(buffer, buffer_w.Count);
+    }
+
+    public static byte[] ReceiveDataUntilHeader(UsusamaInterface my_interface)
+    {
+      byte indata = my_interface.ReadOneByteData();
+      // HEAD_BYTE以外は無視
+      if (indata != UsusamaProtocol.HEAD_BYTE)
+      {
+        return null;
+      }
+      // HEAD_BYTEを受信したらパケットを受信
+      else if (indata == HEAD_BYTE)
+      {
+        int size = my_interface.ReadOneByteData();
+        byte[] buffer = new byte[16];
+        if (size < buffer.Length)
+        {
+          my_interface.ReadBytes(buffer, size);
+          //ProcessingReceivedData(buffer, size);
+        }
+        return buffer;
+      }
+      return null;
+    }
+
+    public static UsusamaData ProcessingReceivedData(byte[] buffer)
+    {
+      if (buffer == null)
+      {
+        UsusamaData data_t;
+        data_t.data = 0;
+        data_t.reg = 0;
+        data_t.valid = false;
+        return data_t;
+      }
+      int index = 0;
+      byte reg = buffer[0];
+      index++;
+      byte checksum = 0;
+
+      checksum += reg;
+      byte[] bytes = new byte[4] { 0, 0, 0, 0 };
+
+      for (int i = 0; i < 4; ++i)
+      {
+        byte d = buffer[index++];
+        if (d == ESCAPE_BYTE)
+        {
+          byte nextByte = buffer[index++];
+          bytes[i] = (byte)((int)nextByte ^ (int)ESCAPE_MASK);
+          checksum += (byte)((int)d + (int)nextByte);
+        }
+        else
+        {
+          bytes[i] = d;
+          checksum += d;
+        }
+      }
+
+      byte checksum_recv = buffer[index++];
+      int DATA = 0x00;
+      for (int i = 0; i < 4; i++)
+      {
+        DATA |= (((int)bytes[i]) << (24 - (i * 8)));
+      }
+
+      if (checksum == checksum_recv)
+      {
+        UsusamaData data_t;
+        data_t.data = DATA;
+        data_t.reg = reg;
+        data_t.valid = true;
+        return data_t;
+      }
+      else
+      {
+        // data error
+        Console.WriteLine("data error, checksum is wrong.");
+        UsusamaData data_t;
+        data_t.data = 0;
+        data_t.reg = 0;
+        data_t.valid = false;
+        return data_t;
+      }
+
+    }
+  }
+
+  // Serialなどのインターフェースを抽象化したclass
+  public class UsusamaInterface
+  {
+    public UsusamaInterface()
+    {
+
+    }
+
+    public virtual void DiscardInBuffer() { }
+    public virtual void Close() { }
+    public virtual void SendByteData(byte data) { }
+    public virtual void SendBytes(byte[] buffer, int size) { }
+
+    public virtual byte ReadOneByteData() { return 0; }
+    public virtual void ReadBytes(byte[] buffer, int size) { }
+  }
+
+
+  // UsusamaInterfaceを継承したSerialの実体class
+  public class UsusamaSerial : UsusamaInterface
+  {
+    public static SerialPort port;
+    public UsusamaSerial(string port_name, int baud_rate)
+    {
+      port = new SerialPort(port_name, baud_rate, Parity.None, 8, StopBits.One);
+      port.ReadBufferSize = 32;
+      try
+      {
+        port.Open();
+        port.DtrEnable = true;
+        port.RtsEnable = true;
+        Console.WriteLine("Connected.");
+      }
+      catch (Exception err)
+      {
+        Console.WriteLine("Unexpected exception : {0}", err.ToString());
+      }
+    }
+
+    public override void Close()
+    {
+      port.Close();
+      port.Dispose();
+    }
+
+    public override void DiscardInBuffer()
+    {
+      port.DiscardInBuffer();
+    }
+
+    public override byte ReadOneByteData()
+    {
+      int data = port.ReadByte();
+      if (data < 0)
+      {
+        return 0; // fault
+      }
+      return (byte)data;
+    }
+    public override void ReadBytes(byte[] buffer, int size)
+    {
+      port.Read(buffer, 0, size);
+    }
+    public override void SendByteData(byte data)
+    {
+      WriteOneByteData(data);
+    }
+    public override void SendBytes(byte[] buffer, int size)
+    {
+      port.Write(buffer, 0, size);
+    }
+
+    private void WriteOneByteData(byte data)
+    {
+      byte[] buffer = { data, 0 };
+      port.Write(buffer, 0, 1);
+    }
+
+  }
+}
