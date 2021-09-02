4880523e319040cbc7fdfcf2158cb6e534a0d1fa
佐藤陽威 <sgrsn1711@gmail.com>
2021-09-02 21:28:00 +0900

    chnage routes

--- ususama_serial_demo/ususama_serial_demo/ususama_routes.cs
+++ ususama_serial_demo/ususama_serial_demo/ususama_routes.cs
@@ -37,21 +37,17 @@ namespace ususama_routes
     public List<Pose2D> home_seq = new List<Pose2D>();
     public UsusamaRoutes()
     {
-      // seq1: ゴミ1(仮に芯とする)をゴミ箱に入れるタスク
-      move_seq_1.Add(new Pose2D(0000f / 1000, 000f / 1000, 0.00f, CleanState.Move)); // ホームポジション
-      move_seq_1.Add(new Pose2D(1250f / 1000, 000f / 1000, 0.00f, CleanState.Move)); // まっすぐ進む 芯回収
-      move_seq_1.Add(new Pose2D(1250f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 90deg 旋回
 
       // seq1: ゴミ1(仮に芯とする)をゴミ箱に入れるタスク
       move_seq_1.Add(new Pose2D(0000f / 1000, 000f / 1000, 0.00f, CleanState.Move)); // ホームポジション
-      move_seq_1.Add(new Pose2D(1250f / 1000, 000f / 1000, 0.00f, CleanState.Move)); // まっすぐ進む 芯回収
-      move_seq_1.Add(new Pose2D(1250f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 90deg 旋回
-
-      // ここでファンをオン
-
-      move_seq_2.Add(new Pose2D(1050f / 1000, 150f / 1000, 1.57f, CleanState.Move)); // 少し吹き飛ばす
-      move_seq_2.Add(new Pose2D(1200f / 1000, 150f / 1000, 1.57f, CleanState.Move)); // 水にあたらないようスライド
-      move_seq_2.Add(new Pose2D(1200f / 1000, 850f / 1000, 1.57f, CleanState.Move)); // ゴミ箱に入れる
+      move_seq_1.Add(new Pose2D(1150f / 1000, 000f / 1000, 0.00f, CleanState.Move)); // まっすぐ進む 芯回収
+      move_seq_1.Add(new Pose2D(1150f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 90deg 旋回
+      move_seq_1.Add(new Pose2D(1150f / 1000, 850f / 1000, 1.57f, CleanState.Move)); // ゴミ箱に入れる
+      move_seq_1.Add(new Pose2D(1150f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 戻ってくる
+      move_seq_1.Add(new Pose2D(1050f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // スライド
+      move_seq_1.Add(new Pose2D(1050f / 1000, 200f / 1000, 1.57f, CleanState.Move)); // ちょっと進む
+      move_seq_1.Add(new Pose2D(1050f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 戻ってくる
+      move_seq_1.Add(new Pose2D(1200f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // スライド
 
       // seq3: 便器前の床、便器を掃除するタスク
       move_seq_3.Add(new Pose2D(1200f / 1000, 450f / 1000, 1.57f, CleanState.Move)); // トイレの前に移動
@@ -61,10 +57,10 @@ namespace ususama_routes
 
       // seq4: ゴミ2(仮に紙コップとする)をゴミ箱に入れるタスク
       move_seq_4.Add(new Pose2D(1200f / 1000, 450f / 1000, 3.14f, CleanState.Move)); // 近づく
-      move_seq_4.Add(new Pose2D(1200f / 1000, 450f / 1000, 2.50f, CleanState.Move)); // 紙コップに向きを変更
-      move_seq_4.Add(new Pose2D( 950f / 1000, 780f / 1000, 2.50f, CleanState.Move)); // 紙コップ回収
+      move_seq_4.Add(new Pose2D(1200f / 1000, 450f / 1000, 2.30f, CleanState.Move)); // 紙コップに向きを変更
+      move_seq_4.Add(new Pose2D( 950f / 1000, 780f / 1000, 2.30f, CleanState.Move)); // 紙コップ回収
       move_seq_4.Add(new Pose2D( 950f / 1000, 780f / 1000, 1.57f, CleanState.Move)); // 姿勢をゴミ箱に垂直に変更
-      move_seq_4.Add(new Pose2D(1200f / 1000, 650f / 1000, 1.57f, CleanState.Move)); // 紙コップ横移動
+      move_seq_4.Add(new Pose2D(1200f / 1000, 550f / 1000, 1.57f, CleanState.Move)); // 紙コップ横移動
       move_seq_4.Add(new Pose2D(1200f / 1000, 750f / 1000, 1.57f, CleanState.Move)); // 紙コップゴミ箱に入れる
       move_seq_4.Add(new Pose2D(1200f / 1000, 450f / 1000, 1.57f, CleanState.Move)); // 戻る
 
