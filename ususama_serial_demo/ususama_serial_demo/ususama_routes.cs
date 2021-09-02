1ce9b5227dc97bcd03d2ce202d93ee9bdbe50ea5
佐藤陽威 <sgrsn1711@gmail.com>
2021-09-02 21:56:16 +0900

    Fix route to clean benki

--- ususama_serial_demo/ususama_serial_demo/ususama_routes.cs
+++ ususama_serial_demo/ususama_serial_demo/ususama_routes.cs
@@ -44,14 +44,14 @@ namespace ususama_routes
       move_seq_1.Add(new Pose2D(1150f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 90deg 旋回
       move_seq_1.Add(new Pose2D(1150f / 1000, 850f / 1000, 1.57f, CleanState.Move)); // ゴミ箱に入れる
       move_seq_1.Add(new Pose2D(1150f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 戻ってくる
-      move_seq_1.Add(new Pose2D(1050f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // スライド
-      move_seq_1.Add(new Pose2D(1050f / 1000, 200f / 1000, 1.57f, CleanState.Move)); // ちょっと進む
-      move_seq_1.Add(new Pose2D(1050f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 戻ってくる
-      move_seq_1.Add(new Pose2D(1260f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // スライド
+      //move_seq_1.Add(new Pose2D(1050f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // スライド
+      //move_seq_1.Add(new Pose2D(1050f / 1000, 200f / 1000, 1.57f, CleanState.Move)); // ちょっと進む
+      //move_seq_1.Add(new Pose2D(1050f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // 戻ってくる
+      move_seq_1.Add(new Pose2D(1300f / 1000, 000f / 1000, 1.57f, CleanState.Move)); // スライド
 
       // seq3: 便器前の床、便器を掃除するタスク
-      move_seq_3.Add(new Pose2D(1260f / 1000, 450f / 1000, 1.57f, CleanState.Move)); // トイレの前に移動
-      move_seq_3.Add(new Pose2D(1260f / 1000, 450f / 1000, 3.14f, CleanState.Move)); // トイレ正面となるように旋回
+      move_seq_3.Add(new Pose2D(1300f / 1000, 450f / 1000, 1.57f, CleanState.Move)); // トイレの前に移動
+      move_seq_3.Add(new Pose2D(1300f / 1000, 450f / 1000, 3.14f, CleanState.Move)); // トイレ正面となるように旋回
 
       // ここで床掃除、便器掃除
 
